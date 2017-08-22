d79dc55ebb379447b7207da85e6fd2507f5c7055
Gobli989 <hegeruben@gmail.com>
2017-08-22 17:49:50 +0200

    Update MainClass.java

--- me/gobli989/extremeplayerinfo/MainClass.java
+++ me/gobli989/extremeplayerinfo/MainClass.java
@@ -4,10 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainClass extends JavaPlugin implements Listener {
+public class MainClass extends JavaPlugin{
 	
 	public void onEnable() {
 		getConfig().options().copyDefaults(true);
@@ -21,44 +20,60 @@ public class MainClass extends JavaPlugin implements Listener {
 				if(getConfig().getBoolean("settings.Enable")) {
 					if(player.hasPermission("extremecore.playerinfo")) {
 						if(args.length == 0) {
-							player.sendMessage(ChatColor.RED+"Usage: /pi <Target>");
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+ChatColor.RED+getConfig().getString("messages.misc.Usage"));
 						} else {
 							Player target = getServer().getPlayer(args[0]);
-							player.sendMessage(ChatColor.WHITE+"["+ChatColor.DARK_AQUA+"----------------"+ChatColor.WHITE+"] PlayerInfo ["+ChatColor.DARK_AQUA+"----------------"+ChatColor.WHITE+"]");
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Name")+": "+ChatColor.DARK_AQUA+target.getName());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.DisplayName")+": "+ChatColor.DARK_AQUA+target.getDisplayName());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.UUID")+": "+ChatColor.DARK_AQUA+target.getUniqueId());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Gamemode")+": "+ChatColor.DARK_AQUA+target.getGameMode());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.World")+": "+ChatColor.DARK_AQUA+target.getWorld().getName());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.LocX")+": "+ChatColor.DARK_AQUA+target.getLocation().getBlockX());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.LocY")+": "+ChatColor.DARK_AQUA+target.getLocation().getBlockY());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.LocZ")+": "+ChatColor.DARK_AQUA+target.getLocation().getBlockZ());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Food")+": "+ChatColor.DARK_AQUA+target.getFoodLevel());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Health")+": "+ChatColor.DARK_AQUA+target.getHealth());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.MaxHealth")+": "+ChatColor.DARK_AQUA+target.getMaxHealth());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Exp")+": "+ChatColor.DARK_AQUA+target.getExp());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Flying")+": "+ChatColor.DARK_AQUA+target.isFlying());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.FlySpeed")+": "+ChatColor.DARK_AQUA+target.getFlySpeed());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.WalkSpeed")+": "+ChatColor.DARK_AQUA+target.getWalkSpeed());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Banned")+": "+ChatColor.DARK_AQUA+target.isBanned());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Whitelisted")+": "+ChatColor.DARK_AQUA+target.isWhitelisted());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Online")+": "+ChatColor.DARK_AQUA+target.isOnline());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Op")+": "+ChatColor.DARK_AQUA+target.isOp());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Sneaking")+": "+ChatColor.DARK_AQUA+target.isSneaking());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Sprint")+": "+ChatColor.DARK_AQUA+target.isSprinting());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Sleeping")+": "+ChatColor.DARK_AQUA+target.isSleeping());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Dead")+": "+ChatColor.DARK_AQUA+target.isDead());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Blocking")+": "+ChatColor.DARK_AQUA+target.isBlocking());
-							player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.InsideVehicle")+": "+ChatColor.DARK_AQUA+target.isInsideVehicle());
-							if(player.hasPermission("extremeplayerinfo.info.ip")) {
-								player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Ip")+": "+ChatColor.DARK_AQUA+target.getAddress());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Header")));
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Name")+": "+target.getName());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.DisplayName")+": "+target.getDisplayName());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.UUID")+": "+target.getUniqueId());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Gamemode")+": "+target.getGameMode());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.World")+": "+target.getWorld().getName());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.LocX")+": "+target.getLocation().getBlockX());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.LocY")+": "+target.getLocation().getBlockY());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.LocZ")+": "+target.getLocation().getBlockZ());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Food")+": "+target.getFoodLevel());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Health")+": "+target.getHealth());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Exp")+": "+target.getExp());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Flying")+": "+target.isFlying());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.FlySpeed")+": "+target.getFlySpeed());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.WalkSpeed")+": "+target.getWalkSpeed());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Banned")+": "+target.isBanned());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Whitelisted")+": "+target.isWhitelisted());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Online")+": "+target.isOnline());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Op")+": "+target.isOp());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Sneaking")+": "+target.isSneaking());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Sprint")+": "+target.isSprinting());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Sleeping")+": "+target.isSleeping());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Dead")+": "+target.isDead());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Blocking")+": "+target.isBlocking());
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.InsideVehicle")+": "+target.isInsideVehicle());
+							if(player.hasPermission("extremeplayerinfo.info.Ip")) {
+								player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Ip")+": "+target.getAddress());
 							} else {
-								player.sendMessage(ChatColor.WHITE+getConfig().getString("messages.info.Ip")+": "+ChatColor.DARK_AQUA+getConfig().getString("messages.info.Secret"));
+								player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.info.Ip")+": "+getConfig().getString("messages.info.Secret"));
 							}
-							player.sendMessage(ChatColor.WHITE+"["+ChatColor.DARK_AQUA+"----------------"+ChatColor.WHITE+"] PlayerInfo ["+ChatColor.DARK_AQUA+"----------------"+ChatColor.WHITE+"]");
 						}
+					} else {
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix")+" "+getConfig().getString("messages.misc.No-Perm")));
 					}
+				} else {
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix")+" "+getConfig().getString("messages.misc.Not-Enabled")));
 				}
+			} else {
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix")+" "+getConfig().getString("messages.misc.Only-Player")));
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("extremeplayerinfo")) {
+			if(sender.hasPermission("extremeplayerinfo.reload")) {
+				if(getConfig().getBoolean("settings.Reload-Config") == true) {
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.misc.Config-Reload"));
+					reloadConfig();
+				} else {
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.misc.No-Reload"));
+				}
+			} else {
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("settings.Prefix"))+" "+getConfig().getString("messages.misc.No-Perm"));
 			}
 		}
 		return false;
